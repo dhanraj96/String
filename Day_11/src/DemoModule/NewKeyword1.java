3cf914a2902617a0babf06b1d103ff23612c3cf5
Admin <Admin@DESKTOP-7E78KSE>
2022-02-24 19:10:21 +0530

    String in Java

---
+++ Day_11/src/DemoModule/NewKeyword1.java
@@ -0,0 +1,20 @@
+package DemoModule;
+
+
+
+public class NewKeyword1 {
+
+	public static void main(String[] args) {
+		String a = new String("Dhanraj");
+		System.out.println(a);
+		String b = new String("Dhanraj");
+		System.out.println(b);
+		a = a.concat("Nimbalkar");
+		System.out.println(a);
+		
+		
+		
+
+	}
+
+}
---
+++ Day_11/src/DemoModule/StringLiteral1.java
@@ -0,0 +1,18 @@
+package DemoModule;
+
+public class StringLiteral1 {
+
+	public static void main(String[] args) {
+		String a = "Dhanraj";
+		System.out.println(a);
+		String b = "Dhanraj";
+		System.out.println(b);
+		a = a.concat("Nimbalkar");
+		System.out.println(a);
+		
+		
+		
+
+	}
+
+}
